01454a5c291402c2ac5614bb39258d268c1562e5
adsbrar <adsbrar@users.noreply.github.com>
2017-09-25 01:12:33 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
-var port = 8080;
+var port = process.env.PORT || 8080;
 
 var server = app.listen(port, function(req, res){
 	console.log("Server initialized");
