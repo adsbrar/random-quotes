c05a111ba59d53e2f597fe479009424099404bd2
adsbrar <adsbrar@users.noreply.github.com>
2017-09-25 00:18:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+var quotes = require('./quotes');
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+var port = 3000;
+
+var server = app.listen(port, function(req, res){
+	console.log("Server initialized");
+});
+
